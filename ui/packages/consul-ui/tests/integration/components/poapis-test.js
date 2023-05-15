2b8e3772da14a12ec9805e9d946dd2ec1059b304
Aditya <aditya.j@protectonce.com>
2023-05-15 18:27:19 +0530

    draft 1 with all integration

--- ui/packages/consul-ui/tests/integration/components/poapis-test.js
+++ ui/packages/consul-ui/tests/integration/components/poapis-test.js
@@ -3,22 +3,22 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | Component | poapislist', function (hooks) {
+module('Integration | Component | poapis', function (hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function (assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`<Poapislist />`);
+    await render(hbs`<Poapis />`);
 
     assert.dom(this.element).hasText('');
 
     // Template block usage:
     await render(hbs`
-      <Poapislist>
+      <Poapis>
         template block text
-      </Poapislist>
+      </Poapis>
     `);
 
     assert.dom(this.element).hasText('template block text');
